e06c20695d51471aa5b28a3e5c903bd3cbed9636
Jakob Mittelberger <jakob.mittelberger@student.uibk.ac.at>
2019-11-03 14:16:59 +0100

    Added nonBlocking + csv

--- proseminar/04/task2/heat_stencil_3D_noBlock.c
+++ proseminar/04/task2/heat_stencil_3D_noBlock.c
@@ -48,10 +48,7 @@ int main(int argc, char **argv) {
     N = atoi(argv[1]);
   }
   int T = N * 10;
-  
-  if(myrank == lastProcess){
-    //printf("Computing heat-distribution for room size N=%d for T=%d timesteps\n", N, T);
-  }
+  //printf("Computing heat-distribution for room size N=%d for T=%d timesteps\n", N, T);
 
   // ---------- setup ----------
 
@@ -117,25 +114,33 @@ int main(int argc, char **argv) {
     Cube H = A;
     A = B;
     B = H;
-
-    if(myrank % 2 == 0){
-      if(myrank != 0){
-        MPI_Send(&A[NPerSlot*myrank][0][0], N*N, MPI_DOUBLE, myrank-1, TAG_L, MPI_COMM_WORLD);
-        MPI_Recv(&A[NPerSlot*myrank-1][0][0], N*N, MPI_DOUBLE, myrank-1, TAG_R, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      }
-      if(myrank != lastProcess){
-        MPI_Send(&A[NPerSlot*(myrank+1)-1][0][0], N*N, MPI_DOUBLE, myrank+1, TAG_R, MPI_COMM_WORLD);
-        MPI_Recv(&A[NPerSlot*(myrank+1)][0][0], N*N, MPI_DOUBLE, myrank+1, TAG_L, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      }
-    } else {
-      if(myrank != lastProcess){
-        MPI_Recv(&A[NPerSlot*(myrank+1)][0][0], N*N, MPI_DOUBLE, myrank+1, TAG_L, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Send(&A[NPerSlot*(myrank+1)-1][0][0], N*N, MPI_DOUBLE, myrank+1, TAG_R, MPI_COMM_WORLD);
-      }
-      if(myrank != 0){
-        MPI_Recv(&A[NPerSlot*myrank-1][0][0], N*N, MPI_DOUBLE, myrank-1, TAG_R, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Send(&A[NPerSlot*myrank][0][0], N*N, MPI_DOUBLE, myrank-1, TAG_L, MPI_COMM_WORLD);
-      }
+    
+    MPI_Request requestL;
+    MPI_Request requestR;
+    //Send
+    if(myrank != 0){
+      MPI_Isend(&A[NPerSlot*myrank][0][0], N*N, MPI_DOUBLE, myrank-1, TAG_L, MPI_COMM_WORLD, &requestL);
+    }
+    
+    if(myrank != lastProcess){
+      MPI_Isend(&A[NPerSlot*(myrank+1)-1][0][0], N*N, MPI_DOUBLE, myrank+1, TAG_R, MPI_COMM_WORLD, &requestR);
+    }
+    
+    //Recive
+    if(myrank != 0){
+      MPI_Recv(&A[NPerSlot*myrank-1][0][0], N*N, MPI_DOUBLE, myrank-1, TAG_R, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    if(myrank != lastProcess){
+      MPI_Recv(&A[NPerSlot*(myrank+1)][0][0], N*N, MPI_DOUBLE, myrank+1, TAG_L, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    
+    
+    //Waiting for completion
+    if(myrank != 0){
+      MPI_Wait(&requestL, MPI_STATUS_IGNORE);
+    }
+    if(myrank != lastProcess){
+      MPI_Wait(&requestR, MPI_STATUS_IGNORE);
     }
   }
 
